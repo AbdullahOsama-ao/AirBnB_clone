2c91a0a96499af1343148229c0a4eb8f94196ca7
Momen Sameh <momensameh5@gmail.com>
2024-03-12 01:52:13 +0000

    added advanced last?

--- console.py
+++ console.py
@@ -30,11 +30,14 @@ class HBNBCommand(cmd.Cmd):
         "Review"
     }
 
+    # if u dont to see ugly string manipulation
+    # dont look at this method
     def default(self, line):
         """ method to handle default """
         commands = ["all", "show", "destroy", "update", "create", "count"]
         args = line.split(".")
         cmnd = args[1].split("(")
+        final_cmnd = ""
         if len(args) < 2:
             print("** class name missing **")
             return
@@ -46,15 +49,37 @@ class HBNBCommand(cmd.Cmd):
             print(cmnd)
             return
         if cmnd[0] in commands:
+            # if the command is show or destroy
             if cmnd[0] == "destroy" or cmnd[0] == "show":
-                self.onecmd(cmnd[0] + " " + args[0] + " " + cmnd[1][1:-2])
+                final_cmnd = " ".join([cmnd[0], args[0], cmnd[1][1:-2]])
+                self.onecmd(final_cmnd)
                 return
+            # if the command is update
             if cmnd[0] == "update":
-                atr = " ".join([i[1:-1] for i in cmnd[1][0:-1].split(", ")])
-                self.onecmd(cmnd[0] + " " + args[0] + " " + atr)
-                return
+                # split the arguments into the attribute and the value
+                atr = cmnd[1][0:-1].split(", ")
+                # if the value is a dictionary then
+                if len(atr) == 2 and atr[1][0] == "{":
+                    # split the dictionary into key value pairs
+                    dic = atr[1][1:-1].split(": ")
+                    # join the key value pairs into a string with a space
+                    f_dic = " ".join([i for i in dic])
+                    c_id = atr[0][1:-1]
+                    # this just for the line length to be less than 80
+                    final_cmnd = " ".join([cmnd[0], args[0], c_id, f_dic])
+                    # call the update method with the key value pairs
+                    self.onecmd(final_cmnd)
+                # else just do a normal update command
+                else:
+                    # join the attribute and value into a string with a space
+                    final_atrs = " ".join([i[1:-1] for i in atr])
+                    # this just for the line length to be less than 80
+                    final_cmnd = " ".join([cmnd[0], args[0], final_atrs])
+                    # call the update method with the attribute and value
+                    self.onecmd(final_cmnd)
+                    return
             else:
-                self.onecmd(cmnd + " " + args[0])
+                self.onecmd(cmnd[0] + " " + args[0])
 
         else:
             print("*** Unknown syntax: {}".format(line))
